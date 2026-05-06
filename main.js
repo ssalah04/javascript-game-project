37ced1a06fbc29062108e112b72c218e9096ffb5
sahra <sahra-2000@hotmail.co.uk>
2026-05-06 23:55:27 +0100

    obstacle speed and gap position change.

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const gravity = 0.09; // pulls sprite down by
 const jumpPower = -2.9; // negative up positive down
 const floorY = 460; // floor coordinate at 460.
 
-const obstacleSpeed = 3; // 3px per frame speed
+const obstacleSpeed = 5; // 3px per frame speed
 const obstacleWidth = 40; //  40px 
 const gapSize = 150;
 
@@ -38,7 +38,7 @@ function updateSprite(){      ///
 }
 
 function jump(event) {
-  if (event.code === "Space" && !gameOver) {
+  if (event.code === "Space" && !gameOver) { 
     velocityY = jumpPower;
   }
 }
@@ -46,9 +46,9 @@ function jump(event) {
 function updateObstacle() {// 
   obstacleX -= obstacleSpeed;
 
-  if (obstacleX < -obstacleWidth) {
+  if (obstacleX < -obstacleWidth) {  // left side of obstacleX needs to at least -40(in our case) to fully leave the screen if so, restart from 800px
     obstacleX = 800;
-    gapY = Math.floor(Math.random() * 250) + 80;
+    gapY = Math.floor(Math.random() * 350) + 80;  // 
   }
 
   const topHeight = gapY; 
@@ -66,9 +66,9 @@ function updateObstacle() {//
   obstacleBot.style.height = bottomHeight + "px";
 }
 
-function isColliding(object1, object2) {          /// reference 
+function isColliding(object1, object2) {        // logic inspiration reference: https://www.youtube.com/watch?v=wJC1aEpx8Mc
   return (
-    object1.x < object2.x + object2.width &&
+    object1.x < object2.x + object2.width &&    // 
     object1.x + object1.width > object2.x &&
     object1.y < object2.y + object2.height &&
     object1.y + object1.height > object2.y
@@ -107,7 +107,7 @@ function checkCollision() {
   }
 }
 
-function gameLoop() {
+function gameLoop() {     
   if (gameOver) return;
 
   updateSprite();
@@ -117,6 +117,6 @@ function gameLoop() {
   requestAnimationFrame(gameLoop);
 }
 
-addEventListener("keydown", jump);
+window.addEventListener("keydown", jump);   //.window ia a global browser object is not necessary to prefix. Ref: https://developer.mozilla.org/en-US/docs/Web/API/Window/window
 
 gameLoop();
