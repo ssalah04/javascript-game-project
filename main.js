4723f78141a37bb74a027e063117626abe3d628e
sahra <sahra-2000@hotmail.co.uk>
2026-05-07 01:09:33 +0100

    add tileset.png

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const obstacleBot = document.querySelector("#obstacleBot");
 let spriteY = 0;  //  Starting point of sprite. Close to the top.
 let obstacleX = 800; // Starting point right edge of the game area
 let velocityY = 1.3; // speed of sprite falling and jumping
-let gapY = 120;  // modify this to make game easier or harder
+let gapY = 120;  // Starting postion of gap between vertical obstacles. odify this to make game easier or harder 
 
 const spriteX = 100; // sprite is fixed on 100px on the X axis. 
-const spriteWidth = 40; // sprite width
-const spriteHeight = 40; // sprite height
+const spriteWidth = 40; // sprite is 40px in width
+const spriteHeight = 40; // sprite is 40px in height
 
-const gravity = 0.09; // pulls sprite down by 
+const gravity = 0.09; // pulls sprite down by 0.
 const jumpPower = -2.9; // negative up positive down
 const floorY = 460; // floor coordinate at 460.
 
@@ -26,7 +26,7 @@ function updateSprite(){      ///
   velocityY += gravity;
   spriteY += velocityY;
 
-  if (spriteY >= floorY) {
+  if (spriteY >= floorY) { // if the sprite bigger or = to floor coordinate. Stop velocity + game over.
     spriteY = floorY;
     velocityY = 0;
     gameOver = true;
@@ -34,7 +34,7 @@ function updateSprite(){      ///
   }
 
   sprite.style.left = spriteX + "px"; // This turns int into px in CSS. int + string = string
-  sprite.style.top = spriteY + "px";  
+  sprite.style.top = spriteY + "px";  // Same ^^ 
 }
 
 function jump(event) {
@@ -48,11 +48,11 @@ function updateObstacle() {//
 
   if (obstacleX < -obstacleWidth) {  // left side of obstacleX needs to at least -40(in our case) to fully leave the screen if so, restart from 800px
     obstacleX = 800;
-    gapY = Math.floor(Math.random() * 350) + 80;  // 
+    gapY = Math.floor(Math.random() * 350) + 80;  // Math.floor(Math.random()) randomises a coordinate in px  
   }
 
   const topHeight = gapY; 
-  const bottomY = gapY + gapSize;  /// how?? 
+  const bottomY = gapY + gapSize;  /// 
   const bottomHeight = floorY - bottomY + spriteHeight; // bottom of what?? 
 
   obstacleTop.style.left = obstacleX + "px";
@@ -66,7 +66,7 @@ function updateObstacle() {//
   obstacleBot.style.height = bottomHeight + "px";
 }
 
-function isColliding(object1, object2) {        // logic inspiration reference: https://www.youtube.com/watch?v=wJC1aEpx8Mc
+function isColliding(object1, object2) {        // logic understanding reference: https://www.youtube.com/watch?v=wJC1aEpx8Mc
   return (
     object1.x < object2.x + object2.width &&    // 
     object1.x + object1.width > object2.x &&
